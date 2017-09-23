94fe6efe32c2540dfd47e7699bc949ad71e37beb
ashwinkshenoy <ashwinkshenoy@gmail.com>
2017-09-23 19:52:36 +0530

    footer, header split, mob responsiveness

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-  $('.news-slider').slick({
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-prev"></button>'
-  });
+  $('.news-slider').slick();
 });
