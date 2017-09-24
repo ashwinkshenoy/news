1aa474b6532715cf0d08279bdd898612d417f63d
ashwinkshenoy <ashwinkshenoy@gmail.com>
2017-09-24 10:41:34 +0530

    menu and search added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
   $('.news-slider').slick();
+
+  // Search
+  $('#search-icon').bind('click', function() {
+    $('.search').show();
+  });
+
+  $('#search-close').bind('click', function() {
+    $('.search').hide();
+  });
+  // Search End
+
+  // Menu
+  $('#menuOpen').bind('click', function() {
+    $('#menuBar').addClass('menu-open');
+    $('#menuOverlay').show();
+  });
+
+  $('#menuClose').bind('click', function() {
+    $('#menuBar').removeClass('menu-open');
+    $('#menuOverlay').hide();
+  });
+
+  $('#menuOverlay').bind('click', function() {
+    $('#menuBar').removeClass('menu-open');
+    $('#menuOverlay').hide();
+  });
+  // Menu End
 });
